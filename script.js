3e0e4b083f8cf33f1f2f209631968ec19a795367
IkhtiarFrontEnd <akhtiar781@gmail.com>
2026-03-15 09:20:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple JS to handle cart button click (placeholder)
+document.querySelectorAll('.product-card .btn').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Product added to cart!');
+    });
+  });
